27a0e9d44765285f584f232421b1074bd141cf9c
Alex Mok <mokalexander00@gmail.com>
2019-06-20 22:08:54 -0400

    feat(collision):combined posenet and 3js

--- index.js
+++ index.js
@@ -1,207 +1,271 @@
-import * as ml5 from 'ml5'
-import * as THREE from 'three'
-import loopThroughPoses from './threeJs/nose'
+/* eslint-disable complexity */
+import * as ml5 from 'ml5';
+import * as THREE from 'three';
+import loopThroughPoses from './threeJs/nose';
 
-let video = document.createElement('video')
-let vidDiv = document.getElementById('video')
-let leftEyeButton = document.getElementById('leftEye')
-let rightEyeButton = document.getElementById('rightEye')
-let noseButton = document.getElementById('nose')
-let mouthButton = document.getElementById('mouth')
-video.setAttribute('width', 255);
-video.setAttribute('height', 255);
-video.autoplay = true
-vidDiv.appendChild(video)
+let keyboard = {};
+let player = { height: 1.8, speed: -0.5 };
+
+let video = document.createElement('video');
+let vidDiv = document.getElementById('video');
+
+video.setAttribute('width', 200);
+video.setAttribute('height', 200);
+video.autoplay = true;
+// vidDiv.appendChild(video);
 
 // get the users webcam stream to render in the video
-navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-    .then(function(stream) {
-        video.srcObject = stream;
-        // video.hiddend();
-    })
-    .catch(function(err) {
-        console.log("An error occurred! " + err);
-    });
+navigator.mediaDevices
+  .getUserMedia({ video: true, audio: false })
+  .then(function(stream) {
+    video.srcObject = stream;
+    // video.hiddend();
+  })
+  .catch(function(err) {
+    console.log('An error occurred! ' + err);
+  });
 
-let options = { 
+let options = {
   flipHorizontal: true,
-  minConfidence: 0.5
- } 
+  minConfidence: 0.5,
+};
 
-let poseNet = ml5.poseNet(video, options, modelReady)
+let poseNet = ml5.poseNet(video, options, modelReady);
 
-  //three.js code
+//three.js code
 const scene = new THREE.Scene();
-scene.background = new THREE.Color( 0xf0f0f0 );
+scene.background = new THREE.Color(0xf0f0f0);
 // Create a basic perspective camera
-const camera = new THREE.PerspectiveCamera( 75, (window.innerWidth/2) /(window.innerHeight/2), 0.1, 1000 );
-camera.position.z = 20;
-let i = 0;
-camera.position.set(  0, 7, 15 );
-camera.lookAt( scene.position );
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
-// Create a renderer with Antialiasing
-const renderer = new THREE.WebGLRenderer({antialias:true});
+camera.position.set(30, 10, 300);
+// camera.lookAt(scene.position);
 
-// Configure renderer clear color
-renderer.setClearColor("#2E2B40");
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 
-// Configure renderer size
-renderer.setSize(window.innerWidth/2, window.innerHeight/2);
+renderer.setClearColor('#2E2B40');
 
-// Append Renderer to DOM
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
 
+document.body.appendChild(renderer.domElement);
 
-let newSphereGeo = false
-let rightEyeBool = false
-let noseBool = false
-let mouthBool = false
+let geometry = new THREE.BoxGeometry(8, 8);
 
-let geometry = new THREE.BoxGeometry( 1, 1, 1 );
-let sphereGeo = new THREE.SphereGeometry(1, 50, 50, 0, Math.PI * 2, 0, Math.PI * 2);
-let mouthGeo =  new THREE.TorusGeometry( 1, 0.5, 6, 100 );
-let halfMouth = new THREE.BoxGeometry( 5, 1, 1 );
-let material = new THREE.MeshPhongMaterial( { color: "0x2194ce" } );
+let material = new THREE.MeshPhongMaterial({ color: '0x2194ce' });
 
-let cube01 = new THREE.Mesh( geometry, material );
-let cube02 = new THREE.Mesh( sphereGeo, material );
-let cube03 = new THREE.Mesh( sphereGeo, material );
-let oMouth = new THREE.Mesh( mouthGeo, material );
+let box = new THREE.Mesh(geometry, material);
 // let halfMouthObj = new THREE.Mesh( halfMouth, material );
 
+let light = new THREE.PointLight(0xffff00);
+light.position.set(-10, 0, 10);
 
-let  light = new THREE.PointLight( 0xFFFF00 );
-light.position.set( -10, 0, 10 );
-
-function createHemisphereLight() { 
-  return new THREE.HemisphereLight(0x303F9F, 0x000000, 1); 
+function createHemisphereLight() {
+  return new THREE.HemisphereLight(0x303f9f, 0x000000, 1);
 }
 var loader = new THREE.TextureLoader();
-var groundTexture = loader.load( 'https://img.freepik.com/free-photo/white-marble-texture-with-natural-pattern-for-background-or-design-art-work_24076-186.jpg?size=338&ext=jpg' );
-				groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
-				groundTexture.repeat.set( 25, 25 );
-				groundTexture.anisotropy = 16;
-				var groundMaterial = new THREE.MeshLambertMaterial( { map: groundTexture } );
-				var mesh = new THREE.Mesh( new THREE.PlaneBufferGeometry( 20000, 20000 ), groundMaterial );
-				mesh.position.y = - 250;
-				mesh.rotation.x = - Math.PI / 2;
-				mesh.receiveShadow = true;
-				scene.add( mesh );
-			
-scene.add(light, cube01, cube02, cube03, oMouth, createHemisphereLight());
+var groundTexture = loader.load(
+  'https://img.freepik.com/free-photo/white-marble-texture-with-natural-pattern-for-background-or-design-art-work_24076-186.jpg?size=338&ext=jpg'
+);
+groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
+groundTexture.repeat.set(25, 25);
+groundTexture.anisotropy = 16;
+
+// creates floor plane
+let floor = new THREE.Mesh(
+  new THREE.PlaneGeometry(window.innerWidth, window.innerHeight, 100, 100),
+  // wireframe tests for plane geometry which side is the right side
+  new THREE.MeshBasicMaterial({ color: 0x883333, wireframe: true })
+);
+
+// puts the floor along the x-axis
+floor.rotation.x -= Math.PI / 2;
+scene.add(floor);
+
+box.position.y += 10;
+
+scene.add(light, box, createHemisphereLight());
 
 // Render Loop
-
 let lastXPosition = 100;
 let lastYPosition = 100;
 let changeX = 1;
 let changeY = 1;
 
 const changeYXPosition = (faceObj, shape, leftEyeShape, rightEyeShape) => {
- 
+  changeX = faceObj.x - lastXPosition;
+  changeY = faceObj.y - lastYPosition;
 
-  changeX = faceObj.x - lastXPosition
-  changeY = faceObj.y - lastYPosition
-  
-  console.log('changes x,y', changeX, changeY)
-  shape.position.x += (changeX * 0.20)
-  shape.position.y += -(changeY * 0.20)
-  rightEyeShape.position.x = shape.position.x + 3
-  rightEyeShape.position.y = shape.position.y + 4
-  leftEyeShape.position.x = shape.position.x - 3
-  leftEyeShape.position.y = shape.position.y + 4
-  oMouth.position.x = shape.position.x
-  oMouth.position.y = shape.position.y - 4
+  shape.position.x += changeX * 0.2;
+  shape.position.y += -(changeY * 0.2);
 
-  console.log(`shape position x, y`, shape.position.x, shape.position.y)
-  lastXPosition = faceObj.x
-  lastYPosition = faceObj.y
-  console.log('lastpositions', lastXPosition, lastYPosition)
-}
-// import changeYXPosition from './threeJs/changeXY'
-
-const render = function (aNose, shape, aRightEye, aLeftEye) {
-  newSphereGeo ? aRightEye.geometry = geometry : aRightEye.geometry = sphereGeo
-  rightEyeBool ? aLeftEye.geometry = geometry : aLeftEye.geometry = sphereGeo
-  noseBool ? cube01.geometry = geometry : cube01.geometry = sphereGeo
-  mouthBool ? oMouth.geometry =  mouthGeo : oMouth.geometry = halfMouth
-  // if (newSphereGeo) {
-  //   aRightEye.geometry = geometry
-  // } else {
-  //   aRightEye.geometry = sphereGeo
-  // }
-  console.log(newSphereGeo)
-  changeYXPosition(aNose, shape, aRightEye, aLeftEye)
-  aRightEye.rotation.x += 0.1
-  aLeftEye.rotation.x -= 0.1
-  
- 
-  renderer.render(scene, camera);
-
+  lastXPosition = faceObj.x;
+  lastYPosition = faceObj.y;
 };
 
-let nose = {}
-let rightEye = {}
-let leftEye = {}
+const render = function(aNose, shape) {
+  changeYXPosition(aNose, shape);
 
-poseNet.on('pose',  function(results) {
+  camera.position.z -= 1;
+
+  if (keyboard[87]) {
+    camera.position.x += Math.sin(camera.rotation.y) * player.speed;
+    camera.position.z += Math.cos(camera.rotation.y) * player.speed;
+  }
+  // S KEY move backward
+  if (keyboard[83]) {
+    camera.position.x -= Math.sin(camera.rotation.y) * player.speed;
+    camera.position.z -= Math.cos(camera.rotation.y) * player.speed;
+  }
+  // W key strafe left
+  if (keyboard[65]) {
+    camera.position.x -=
+      Math.sin(camera.rotation.y - Math.PI / 2) * player.speed;
+    camera.position.z -=
+      Math.cos(camera.rotation.y - Math.PI / 2) * player.speed;
+  }
+  // D key strafe right
+  if (keyboard[68]) {
+    camera.position.x +=
+      Math.sin(camera.rotation.y - Math.PI / 2) * player.speed;
+    camera.position.z +=
+      Math.cos(camera.rotation.y - Math.PI / 2) * player.speed;
+  }
+
+  // Left Arrow rotate left
+  if (keyboard[37]) {
+    camera.rotation.y += Math.PI * 0.01;
+  }
+  // Right Arrow rotate right
+  if (keyboard[39]) {
+    camera.rotation.y -= Math.PI * 0.01;
+  }
+
+  if (
+    col1.position.x + 10 >
+    box.position.x - box.geometry.parameters.width / 2
+  ) {
+    console.log('HITTTT1', col1.position.x);
+  }
+
+  if (
+    col2.position.x - 10 <
+    box.position.x + box.geometry.parameters.width / 2
+  ) {
+    console.log('HITTTT2', col2.position.x);
+  }
+
+  if (
+    col3.position.y - 10 <
+    box.position.y + box.geometry.parameters.width / 2
+  ) {
+    console.log('HITTTT3', col2.position.x);
+  }
+
+  renderer.render(scene, camera);
+};
+
+let nose = {};
+let rightEye = {};
+let leftEye = {};
+
+poseNet.on('pose', function(results) {
   let poses = results;
- loopThroughPoses(poses, nose, rightEye, leftEye)
+  loopThroughPoses(poses, nose, rightEye, leftEye);
   let estimatedNose = {
     x: nose.x,
-    y: nose.y
-  }
-  if (estimatedNose.x && estimatedNose.y){
-  console.log("On POSE", estimatedNose.x)
-  render(estimatedNose, cube01, cube02, cube03)
+    y: nose.y,
+  };
+  if (estimatedNose.x && estimatedNose.y) {
+    render(estimatedNose, box);
   }
 });
 
 function modelReady() {
-  
-  console.log("model Loaded")
-
+  console.log('model Loaded');
 }
-// render()
-leftEyeButton.addEventListener('click', function(){
-  if (newSphereGeo) {
-    newSphereGeo = false
-  } else {
-    newSphereGeo = true
-  }
-});
 
-rightEyeButton.addEventListener('click', function(){
-  if (rightEyeBool) {
-    rightEyeBool = false
-  } else {
-    rightEyeBool = true
-  }
-});
+window.addEventListener('resize', onWindowResize, false);
 
-noseButton.addEventListener('click', function(){
-  if (noseBool) {
-    noseBool = false
-  } else {
-    noseBool = true
-  }
-});
-mouthButton.addEventListener('click', function(){
-  if (mouthBool) {
-    mouthBool = false
-  } else {
-    mouthBool = true
-  }
-});
-window.addEventListener( 'resize', onWindowResize, false );
-			
 function onWindowResize() {
-
-   camera.aspect = (window.innerWidth/2) / (window.innerHeight/2);
-   camera.updateProjectionMatrix();
-   renderer.setSize( window.innerWidth/2, window.innerHeight/2 );
+  camera.aspect = window.innerWidth / 2 / (window.innerHeight / 2);
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
   //  video.setAttribute('width', window.innerWidth/2);
   //  video.setAttribute('height', window.innerWidth/2);
 }
 
+//
+//
+//
+//
+
+let wallGeometry = new THREE.BoxGeometry(50, 50);
+let wallMaterial = new THREE.MeshStandardMaterial({
+  color: 0x883322,
+  wireframe: true,
+  side: THREE.DoubleSide,
+});
+let wall = new THREE.Mesh(wallGeometry, wallMaterial);
+
+// let boxGeometry = new THREE.BoxGeometry(8, 8);
+// let boxMaterial = new THREE.MeshStandardMaterial({
+//   color: 0xffffff,
+//   wireframe: false,
+// });
+// let box = new THREE.Mesh(boxGeometry);
+// box.material.color.setHex(0x00ff00)
+
+let col1Geometry = new THREE.BoxGeometry(20, 50);
+let col1Material = new THREE.MeshStandardMaterial({
+  color: 0x883322,
+  wireframe: false,
+  side: THREE.DoubleSide,
+});
+let col1 = new THREE.Mesh(col1Geometry, col1Material);
+
+let col2Geometry = new THREE.BoxGeometry(20, 50);
+let col2Material = new THREE.MeshStandardMaterial({
+  color: 0x883322,
+  wireframe: true,
+  side: THREE.DoubleSide,
+});
+let col2 = new THREE.Mesh(col2Geometry, col2Material);
+
+let col3Geometry = new THREE.BoxGeometry(10, 20);
+let col3Material = new THREE.MeshStandardMaterial({
+  color: 0x883322,
+  wireframe: true,
+  side: THREE.DoubleSide,
+});
+let col3 = new THREE.Mesh(col3Geometry, col3Material);
+
+col1.position.z += 10;
+col1.position.y += 25;
+col2.position.y += 25;
+col2.position.z += 10;
+col2.position.x += 30;
+col3.position.y += 10;
+col3.position.z += 10;
+col3.position.x += 15;
+col3.position.y += 30;
+
+// scene.add(box);
+scene.add(col1);
+scene.add(col2);
+scene.add(col3);
+
+function keyDown(event) {
+  keyboard[event.keyCode] = true;
+}
+
+function keyUp(event) {
+  keyboard[event.keyCode] = false;
+}
+
+window.addEventListener('keydown', keyDown);
+window.addEventListener('keyup', keyUp);
